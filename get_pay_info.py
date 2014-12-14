2820e3a262b52053fe57d936580d28506fa86415
sys <1052856576@qq.com>
2014-12-14 19:43:20 +0800

    add notes

--- get_pay_info.py
+++ get_pay_info.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python
 
+'''
+    name: get_pay_info
+    function: 自动获取流量信息
+    created: @bigeagle
+    modefied: @dby
+    lib: requests, lxml, tesseract
+    parameters:
+        BASE_URL---基本的url，在此网址获取用户名，密码和验证码图片
+        FORM_URL---登陆的url
+        PAY_INFO_URL---获取信息的url
+        USERNAME(username)---用户名
+        PASSOWORD(passoword)---密码
+        checkcode---验证码
+'''
+
 import re
 import time
 import os
@@ -11,7 +26,6 @@ USERNAME = "1401120585"
 PASSOWORD = "190255"
 
 BASE_URL = "http://zyzfw.xidian.edu.cn:8800/"
-
 FORM_URL = "http://zyzfw.xidian.edu.cn:8800/index.php?action=login"
 PAY_INFO_URL = "http://zyzfw.xidian.edu.cn:8800/index.php"
 
@@ -19,6 +33,11 @@ TMP_DIR = os.path.expanduser("~/.xidian/")
 IMG_PATH = os.path.join(TMP_DIR, "img.jpg")
 TEXT_PATH = os.path.join(TMP_DIR, "result.txt")
 
+'''
+    name: make_data_and_cookies
+    function: 获取登陆所需的数据和cookies
+    return: data,cookies
+'''
 def make_data_and_cookies():
     """make the post data(including vcode) and get cookies"""
 
@@ -51,20 +70,28 @@ def make_data_and_cookies():
             }
     return data, r.cookies
 
-
+'''
+    name: submit_form
+    parameters: data,cookies(其意义与上函数类似)
+    function: 模仿form进行登陆
+    return: None
+'''
 def submit_form(data, cookies):
     """submit the login form so you're logined in"""
     form_action_url = FORM_URL
     r = requests.post(form_action_url, data=data, cookies=cookies)
 
-
+'''
+    name: get_info
+    parameters: cookies
+    function: 获取信息,并打印出来
+    return: None
+'''
 def get_info(cookies):
     """retrieve the data using the cookies"""
     info_url = PAY_INFO_URL
     r = requests.get(info_url, cookies=cookies)
     doc = html.document_fromstring(r.text)
-    used_gb = 0
-    rest_gb = 0
     #items = re.findall('<td class="title td_content">(.*?)</td>',r.text, re.S)
     messageList = doc.cssselect('div table tbody tr td')[41].text_content()
     lMsg = messageList.strip().split("\n")
@@ -74,6 +101,9 @@ def get_info(cookies):
 if __name__ == '__main__':
     if not os.path.exists(TMP_DIR):
         os.mkdir(TMP_DIR)
+
+#循环，直至成功才跳出循环
+
     while True:
         data, cookies = make_data_and_cookies()
         submit_form(data, cookies)
